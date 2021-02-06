b073db4d0cafac87abbbdc15a6f639490bb71e5e
jamieboy1337 <jamieboy1337@gmail.com>
2021-02-05 22:44:25 -0800

    kick off the build

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include <engine/BaseEngine.hpp>
+#include <engine/Context.hpp>
+#include <scene/SplashScreen.hpp>
+#include <GLFW/glfw3.h>
+
+using namespace ::monkeysworld::engine::baseengine;
+using ::monkeysworld::engine::Context;
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  GLFWwindow* win = InitializeGLFW(1280, 720, "i see pussy sex intercouse in your future");
+  auto scene = new ::scene::SplashScreen();
+  auto ctx = std::make_shared<Context>(win, scene);
+
+  GameLoop(ctx, win);
+
+  glfwDestroyWindow(win);
+  glfwTerminate();
+  return 0;
 }
