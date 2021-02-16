11e79b911f50e1dd6fd7340a8b2619016741f2fa
jamieboy1337 <jamieboy1337@gmail.com>
2021-02-15 19:33:11 -0800

    swap to engine context now

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <engine/BaseEngine.hpp>
-#include <engine/Context.hpp>
+#include <engine/EngineContext.hpp>
 #include <scene/SplashScreen.hpp>
 #include <GLFW/glfw3.h>
 
 using namespace ::monkeysworld::engine::baseengine;
-using ::monkeysworld::engine::Context;
+using ::monkeysworld::engine::EngineContext;
 
 int main(int, char**) {
   std::cout << "Hello, world!\n";
   GLFWwindow* win = InitializeGLFW(1280, 720, "i see pussy sex intercouse in your future");
   auto scene = new ::scene::SplashScreen();
-  auto ctx = std::make_shared<Context>(win, scene);
+  auto ctx = std::make_shared<EngineContext>(win, scene);
 
   GameLoop(ctx, win);
 
